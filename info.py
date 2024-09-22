c34db9ef19812b693db7b747e09d16d606c5498f
UNBRAKING THINGS <code.pbail@gmail.com>
2024-09-22 11:34:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,7 +21,7 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 USERNAME = environ.get('USERNAME', "https://telegram.me/Pbail")
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002349193496'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/PbailMovieRequestGroup')
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002271528481').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002271528481 -1002059717598').split()]
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://pritamdebnath9898:ZxscpIovGeIanqnU@cluster0.phdll.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "TELEGRAM_BOT_INFO")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
